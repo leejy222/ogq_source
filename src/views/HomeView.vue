13f983536bce613a93f5bc02e641bcdabcff334c
jaeyoungLee <leejy_222@naver.com>
2024-01-09 11:09:31 +0900

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
 
 <template>
   <main>
-    <p class="text-5xl font-bold text-center">OGQ Open Source</p>
+    <p class="text-5xl font-bold text-center">Tree Source</p>
     <p class="text-3xl font-bold text-center mt-7">Lee jae Young</p>
   </main>
 </template>
